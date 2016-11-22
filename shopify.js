a7475ad51ffcb016e94e1de3fcf27e712f8f96b7
neem-sumuga <sumuga@neemtecsolutions.com>
2016-11-22 18:06:17 +0530

    Added Twilio connector

--- shopify.js
+++ shopify.js
@@ -1,47 +1,13 @@
 var Client = require('node-rest-client').Client;
 var client = new Client();
 
+var apiKey,apiPassword,storeName;
 
-module.exports = (function () {
-    function run(req) {
-    	var entity = req.reqData.entity;
-    	var apiKey,apiPassword;
-    	var args = {
-            headers:{Authorization : "Basic "+b64EncodeUnicode(apiKey+":"+apiPassword)}
-        }; 
-        var url="https://"+storeName+".myshopify.com/admin/";
-        var type=entity.type;
-        if(type==customer) {
-        	url+="customers.json";
-   		} else if(type == "product") {
-    		url+="products.json";
-    	} else {
-    		url+="orders.json";
-    	}  
-     	getStoreData(url,args, type, req);  	
-    }
-	   var Shopify = {
-	    init: function (req) {
-	        //initial function to get request data
-	        var credentials = req.reqData.credentials;
-	        apiKey = credentials.apiKey;
-	        apiPassword = credentials.apiPassword;
-	        storeName = credentials.storeName;
-	        run(req);
-	    }	
-	}
-	return Shopify;
-})();
-function post(resArr, req) {
-	console.log("****************RESPONSE**************  %j",resArr);
-	req.reqData.preData = resArr;
-		//post req to the core server
-}
-function getStoreData(url,args,type,req){
+function getStoreData(url,args,type,req) {
 	client.get(url, args, function (data, res) {
-			if(type=="customer"){
+			if(type == "customer") {
 				formCustomer(data.customers,req);
-			} else if (type=="product"){
+			} else if (type == "product") {
 				formProduct(data.products,req);
 			} else {
 				formOrder(data.orders,req)
@@ -50,12 +16,11 @@ function getStoreData(url,args,type,req){
         	console.log('something went wrong on the request', err.request.options);
     });
 }
+
 function b64EncodeUnicode(str) {
-   	/*return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-   		return String.fromCharCode('0x' + p1);
-   	}));*/
-	return new Buffer(str).toString('base64');
+   	return new Buffer(str).toString('base64');
 }
+
 function formCustomer(dataArr, req) {
 	var obj, resObj;
 	var resArr = [];
@@ -80,9 +45,7 @@ function formCustomer(dataArr, req) {
 		addr1.countryISO2 = obj.default_address.country_code;
 		addr1.zip = obj.default_address.zip;
 		addr1.phone = obj.default_address.phone;
-		var addresses = [];
-		addresses[0] = addr1;
-		resObj.addresses = addresses;
+		resObj.defaultAddress = addr1;
 		resArr[i] = resObj;
 		}
 		post(resArr, req);
@@ -90,80 +53,119 @@ function formCustomer(dataArr, req) {
 
 function formOrder(dataArr, req) {
 	var obj, resObj;
-	var resArr=[];
-	for(var i=0;i<dataArr.length;i++) {
-		resObj={};
-		obj=dataArr[i];
-		resObj.id=obj.id;
-		resObj.email=obj.email;
-		resObj.createdAt=obj.created_at;
-		resObj.updatedAt=obj.updated_at;
-		resObj.price=obj.total_price;
-		resObj.status=obj.financial_status;
-		resObj.name=obj.name;
-		resObj.customerId=obj.customer.id;
-		var billingAddress={}
-		billingAddress.name=obj.billing_address.name;
-		billingAddress.street=obj.billing_address.address1;
-		billingAddress.city=obj.billing_address.city;
-		billingAddress.state=obj.billing_address.province;
-		billingAddress.stateISO2=obj.billing_address.province_code;
-		billingAddress.country=obj.billing_address.country;
-		billingAddress.countryISO2=obj.billing_address.countryISO2;
-		billingAddress.zip=obj.billing_address.zip;
-		billingAddress.phone=obj.billing_address.phone;
-		resObj.billingAddress=billingAddress;
-		var shippingAddress={};
-		shippingAddress.name=obj.shipping_address.name;
-		shippingAddress.street=obj.shipping_address.address1;
-		shippingAddress.city=obj.shipping_address.city;
-		shippingAddress.state=obj.shipping_address.province;
-		shippingAddress.stateISO2=obj.shipping_address.province_code;
-		shippingAddress.country=obj.shipping_address.country;
-		shippingAddress.countryISO2=obj.shipping_address.countryISO2;
-		shippingAddress.zip=obj.shipping_address.zip;
-		shippingAddress.phone=obj.shipping_address.phone;
-		resObj.shippingAddress=shippingAddress;
-		resObj.shippingMethod=obj.processing_method;
-		resObj.paymentMethod=obj.processing_method;
-		var items=[];
+	var resArr = [];
+	for(var i = 0; i < dataArr.length; i++) {
+		resObj = {};
+		obj = dataArr[i];
+		resObj.id = obj.id;
+		resObj.email = obj.email;
+		resObj.createdAt = obj.created_at;
+		resObj.updatedAt = obj.updated_at;
+		resObj.price = obj.total_price;
+		resObj.status = obj.financial_status;
+		resObj.name = obj.name;
+		resObj.customerId = obj.customer.id;
+		var billingAddress = {}
+		billingAddress.name = obj.billing_address.name;
+		billingAddress.street = obj.billing_address.address1;
+		billingAddress.city = obj.billing_address.city;
+		billingAddress.state = obj.billing_address.province;
+		billingAddress.stateISO2 = obj.billing_address.province_code;
+		billingAddress.country = obj.billing_address.country;
+		billingAddress.countryISO2 = obj.billing_address.countryISO2;
+		billingAddress.zip = obj.billing_address.zip;
+		billingAddress.phone = obj.billing_address.phone;
+		resObj.billingAddress = billingAddress;
+		var shippingAddress = {};
+		shippingAddress.name = obj.shipping_address.name;
+		shippingAddress.street = obj.shipping_address.address1;
+		shippingAddress.city = obj.shipping_address.city;
+		shippingAddress.state = obj.shipping_address.province;
+		shippingAddress.stateISO2 = obj.shipping_address.province_code;
+		shippingAddress.country = obj.shipping_address.country;
+		shippingAddress.countryISO2 = obj.shipping_address.countryISO2;
+		shippingAddress.zip = obj.shipping_address.zip;
+		shippingAddress.phone = obj.shipping_address.phone;
+		resObj.shippingAddress = shippingAddress;
+		resObj.shippingMethod = obj.processing_method;
+		resObj.paymentMethod = obj.processing_method;
+		var items = [];
 		var itemObj,item;
-		var quantity=0;
-		for(var j=0;j<obj.line_items.length;j++) {
-			item={};
-			itemObj=obj.line_items[j];
-			item.id=itemObj.product_id;
-			item.name=itemObj.name;
-			item.price=itemObj.price;
-			item.quantity=itemObj.quantity;
-			item.sku=itemObj.sku;
-			items[i]=item;
-			quantity+=itemObj.quantity;
+		var quantity = 0;
+		for(var j = 0; j < obj.line_items.length; j++) {
+			item = {};
+			itemObj = obj.line_items[j];
+			item.id = itemObj.product_id;
+			item.name = itemObj.name;
+			item.price = itemObj.price;
+			item.quantity = itemObj.quantity;
+			item.sku = itemObj.sku;
+			items[i] = item;
+			quantity += itemObj.quantity;
 		}
-		resObj.items=items;
-		resObj.quantity=quantity;
-		resArr[i]=resObj;
+		resObj.items = items;
+		resObj.quantity = quantity;
+		resArr[i] = resObj;
 	}
 	post(resArr, req);
 }
 
-function formProduct(dataArr,req){
+function formProduct(dataArr,req) {
 	var obj,resObj;
-	var resArr=[];
-	for(var i=0;i<dataArr.length;i++) {
-		resObj={};
-		obj=dataArr[i];
-		resObj.id=obj.id;
-		resObj.name=obj.title;
-		resObj.createdAt=obj.created_at;
-		resObj.updatedAt=obj.updated_at;
-		resObj.description=obj.body_html;
-		resObj.catagory=obj.product_type;
-		var variants=obj.variants[0];
-		resObj.sku=variants.sku;
-		resObj.price=variants.price;
-		resObj.qtyOnHand=variants.inventory_quantity;
-		resArr[i]=resObj;
+	var resArr = [];
+	for(var i = 0;  i < dataArr.length; i++) {
+		resObj = {};
+		obj = dataArr[i];
+		resObj.id = obj.id;
+		resObj.name = obj.title;
+		resObj.createdAt = obj.created_at;
+		resObj.updatedAt = obj.updated_at;
+		resObj.description = obj.body_html;
+		resObj.catagory = obj.product_type;
+		var variants = obj.variants[0];
+		resObj.sku = variants.sku;
+		resObj.price = variants.price;
+		resObj.qtyOnHand = variants.inventory_quantity;
+		resArr[i] = resObj;
 	}
 	post(resArr, req);
 }
+
+function run(node) {
+	var entity = node.reqData.entity;
+	var args = {
+        headers:{Authorization : "Basic "+b64EncodeUnicode(apiKey+":"+apiPassword)}
+    }; 
+    var url = "https://"+storeName+".myshopify.com/admin/";
+    var type =  entity.type;
+    if(type == "customer") {
+    	url += "customers.json";
+		} else if(type == "product") {
+		url += "products.json";
+	} else {
+		url += "orders.json";
+	}  
+ 	getStoreData(url,args, type, node);  	
+}
+
+function post(resArr, node) {
+	console.log("****************RESPONSE**************  %j",resArr);
+	node.reqData.preData = resArr;
+		//post req to the core server
+}
+
+module.exports = (function () {
+	var Shopify = {
+	   		 init: function (node) {
+	      	  //initial function to get request data
+	       		var credentials = node.reqData.credentials;
+				apiKey = credentials[0];
+	        	apiPassword = credentials[1];
+	        	storeName = credentials[2];
+	        	run(node);
+	    	}	
+		}
+	return Shopify;
+})();
+
+
---
+++ twilio.js
@@ -0,0 +1,67 @@
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+var accountSid,authToken,baseUrl,fromPhone;
+
+
+function run(node) {
+        var postArr = node.reqData.preData;
+        var type=node.reqData.entity.type;
+        var url="https://api.twilio.com/2010-04-01/Accounts/"+accountSid+"/" ;
+        var obj;
+        var resArr=[];
+		for(var i=0;i<postArr.length;i++){
+			obj=postArr[i];
+			var postData="";
+			var message,toPhone;
+			if(obj.hasOwnProperty("shippingAddress")) {
+				toPhone=obj.shippingAddress.phone;
+			} else {
+				toPhone=obj.defaultAddress.phone;
+			}
+			if(type=="call") {
+        		url+="Calls.json";
+        		postData+=encodeURIComponent(baseUrl)
+			} else {
+        		url+="Messages.json";
+        		postData+="Body="+encodeURIComponent(obj.message);
+       		}
+        	postData+="&From="+encodeURIComponent(fromPhone)+"&To="+encodeURIComponent(toPhone);
+			var args = {
+                data :postData,
+                headers : {Authorization : "Basic "+b64EncodeUnicode(accountSid+":"+authToken),Accept : "application/json","Content-Type" : "application/x-www-form-urlencoded"}
+            }; 
+            client.post(url, args, function (data, res) {
+                console.log("Twilio Response: %j", data);
+                resArr[i]=data;
+            }).on('error',function(err) {
+                console.log('something went wrong on the request', err.request.options);
+            });
+		}   
+		post(resArr,node);    
+    }
+    
+    function b64EncodeUnicode(str) {
+        return new Buffer(str).toString('base64');
+    }
+
+    function post(resArr, node) {
+        node.reqData.preData = resArr;
+        //post req to the core server
+    }
+
+module.exports = (function () {
+    var Twilio = {
+        init: function (node) {
+            //initial function to get request data
+            var credentials = node.reqData.credentials;
+           	accountSid= credentials[0];
+            authToken = credentials[1];
+            fromPhone=credentials[2];
+            baseUrl = credentials[3];
+            run(node);
+        }   
+    }
+    return Twilio;
+})();
+
